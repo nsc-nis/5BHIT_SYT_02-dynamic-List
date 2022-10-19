60496870f49a1fd0eb5e97c5c90e2a43908237e6
nsc-nis <niklas.schachl@htl-wels.at>
2022-10-19 10:21:55 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h3>Example</h3>
+    <form>
+      <label>Enter new subject:</label>
+      <input v-model="newSubject" />
+      <button>Add subject</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const newSubject = ref("");
+
+    // expose the state to the template
+    return {
+      newSubject,
+    };
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h3 {
+  margin-bottom: 20px;
+}
+</style>
